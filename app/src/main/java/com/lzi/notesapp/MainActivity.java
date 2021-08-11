dff67211a899ee4c278c639907bcdc6ef29b925c
LoloziAnas <lolozianas1@gmail.com>
2021-08-11 22:28:52 +0100

    test

--- app/src/main/java/com/lzi/notesapp/MainActivity.java
+++ app/src/main/java/com/lzi/notesapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    String s = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
